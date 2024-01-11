8aaa21f1f75e869202a8bdacd87aee159c97db94
Osanyem Osadebe <osanyemosadebe@Osas-Macbook-Pro.local>
2024-01-11 09:03:48 -0500

    full project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+import "./App.css";
+import SearchIcon from "./search.svg";
+
+import MovieCard from "./MovieCard";
+
+// beb91b52
+
+const API_URL = "http://www.omdbapi.com?apikey=beb91b52";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  async function searchForMovie(title) {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  }
+
+  //   useEffect(() => {
+  //     searchForMovie("Batman");
+  //   }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieVerse</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search MovieVerse!"
+          value={searchTerm}
+          onChange={(userInput) => setSearchTerm(userInput.target.value)}
+          onEnter
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => {
+            searchForMovie(searchTerm);
+          }}
+        />
+      </div>
+
+      {movies.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => {
+            return <MovieCard movie={movie} />;
+          })}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 }
